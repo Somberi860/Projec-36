d1226fcca9e0f0b8d4769877223eb4f6faaf20fd
Somberi860 <72584568+Somberi860@users.noreply.github.com>
2021-02-20 10:25:23 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var dog,sadDog,happyDog;
+var foodStock;
+
+
+function preload(){
+  sadDog=loadImage("Images/Dog.png");
+  happyDog=loadImage("Images/happy dog.png");
+}
+
+function setup() {
+  createCanvas(1000,400);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  foodStock = new Food(220,220,50,50);
+
+
+
+}
+
+function draw() {
+  background(46,139,87);
+  drawSprites();
+  foodStock.display();
+}
+
+//function to read food Stock
+
+
+//function to update food stock and last fed time
+
+
+//function to add food in stock
